864ba64cd761dcea70814ddc5fba6e54e628fc44
Tess Lameyer <tjlameyer@gmail.com>
2019-10-21 16:41:48 -0700

    makeBoard written

---
+++ game/make-board-array.js
@@ -0,0 +1,23 @@
+function makeBoard(numRows, numColumns) {
+    const board = []; 
+    for (let i = 0; i < numRows; i++) {
+        const rowsArray = [];
+        board.push(rowsArray);
+
+        for (let j = 0; j < numColumns; j++) {
+            board[i][j] = {};
+
+            const cell = board[i][j];
+            cell.id = i + ',' + j; 
+            cell.row = i; 
+            cell.column = j; 
+            cell.isMine = false;
+            cell.isHidden = true; 
+            cell.isFlagged = false; 
+            cell.numAdjMines = 0; 
+        }
+    }
+    return board; 
+}
+
+console.log(makeBoard(2, 2));
