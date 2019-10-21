bfeda541ac021e850f8b05dd1cbfd056b28dc2e5
Tess Lameyer <tjlameyer@gmail.com>
2019-10-21 15:23:09 -0700

    with comments

--- common/utils.js
+++ common/utils.js
@@ -22,25 +22,24 @@ function getArrayOfMineCoordinates(numMines, numRows, numColumns, firstClickArra
     // initialize an empty array that we will push coordinate pairs into (coorcinate pairs will each be an array of two numbers)
     const arrayOfMineCoordinates = []; 
 
-
+    //generate possible mine coordinates until the number of mine coordinates (set by numMines) is met
     while (arrayOfMineCoordinates.length !== numMines) {
+        //make the random coordinates that we might use
         const rowIndex = generateRandomIndex(numRows);
         const columnIndex = generateRandomIndex(numColumns); 
         const potentialCoordinateArray = [rowIndex, columnIndex]; 
 
-
+        // check for repeats of current mines already in the mine array 
         const coordinateIsNotInArrayOfMineCoordinates = !arrayOfMineCoordinates.find(coordinate => (JSON.stringify(coordinate) === JSON.stringify(potentialCoordinateArray))); 
-        console.log(coordinateIsNotInArrayOfMineCoordinates, 'long name');
-
+        // avoid the one coordinate pair that represents the user's first click on the board
         const isNotFirstClickArray = JSON.stringify(potentialCoordinateArray) !== JSON.stringify(firstClickArray); 
-        console.log(isNotFirstClickArray, 'is not  first click');
 
-
+        // if both of these conditions are met, push the coordinate pair into the array of mines that will actually be used in the game
         if (coordinateIsNotInArrayOfMineCoordinates && isNotFirstClickArray) {
             arrayOfMineCoordinates.push(potentialCoordinateArray); 
-            console.log(potentialCoordinateArray, 'potential coordinate');
         }
     }
+    //output
     return arrayOfMineCoordinates; 
 }
 
