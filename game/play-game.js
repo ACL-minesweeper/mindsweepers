d3348648853a89118a3a30b7efb53267d31c4d89
Libby Hamlin <bravolibby@gmail.com>
2019-10-23 17:45:18 -0700

    improved flag counter

--- game/play-game.js
+++ game/play-game.js
@@ -1,6 +1,6 @@
 // import { flagsRemaining } from './game.js';
 import { isWin, isLoss } from '../common/utils.js';
-const flagDiv = document.getElementById('flag-div');
+const flagDiv = document.getElementById('flag-info');
 let userHasFlag = false;
 flagDiv.addEventListener('click', () => {
     if (userHasFlag) userHasFlag = false;
@@ -11,27 +11,33 @@ flagDiv.addEventListener('click', () => {
 let flagsRemaining = 10;
 // Show user initial amount of flags
 flagDiv.textContent = flagsRemaining;
+const image = document.createElement('img');
+image.src = '../assests/placeholder-baggy.png';
+image.id = 'bag';
+image.alt = 'poop bag icon';
+flagDiv.appendChild(image);
 
 // mine placement are known
 export const playGame = (clickedCellLocationArr, boardArrParam) => {
     const objectRow = clickedCellLocationArr[0];
     const objectColumn = clickedCellLocationArr[1];
     const cellObject = boardArrParam[objectRow][objectColumn];
-    const clickedCellId = clickedCellLocationArr[0] + ',' + clickedCellLocationArr[1];
+    const clickedCellId =
+    clickedCellLocationArr[0] + ',' + clickedCellLocationArr[1];
     const domCell = document.getElementById(clickedCellId);
     if (cellObject.isFlagged) {
         cellObject.isFlagged = false;
-        // update the DOM
+    // update the DOM
         domCell.classList.add('opacity');
         domCell.classList.remove('flagged');
         flagsRemaining++;
         flagDiv.textContent = flagsRemaining;
-    } else
-    // if the user grabbed a flag
-    if (userHasFlag) {
-        // and the cell does not have a flag and the cell is still hidden
+    }
+  // if the user grabbed a flag
+    else if (userHasFlag) {
+    // and the cell does not have a flag and the cell is still hidden
         if (!cellObject.isFlagged && cellObject.isHidden) {
-            // then update the DOM
+      // then update the DOM
             domCell.classList.remove('opacity');
             domCell.classList.add('flagged');
             flagsRemaining--;
@@ -39,17 +45,15 @@ export const playGame = (clickedCellLocationArr, boardArrParam) => {
             cellObject.isFlagged = true;
             userHasFlag = false;
         }
-    } else
-    if (cellObject.isMine) {
-        // this function needs to be created 
+    } else if (cellObject.isMine) {
+    // this function needs to be created
         gameOver();
-    } else 
-    if (cellObject.numAdjMines === 0) {
-        // update the DOM
+    } else if (cellObject.numAdjMines === 0) {
+    // update the DOM
         domCell.classList.remove('opacity');
         cellObject.isHidden = false;
     } else {
-        // populate the DOM with the number
+    // populate the DOM with the number
         domCell.textContent = cellObject.numAdjMines;
         domCell.classList.remove('opacity');
         cellObject.isHidden = false;
