00a38068bb5519e8ceca9ac9902912441bd4a8ba
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-11-25 17:32:49 -0800

    variable directions based on user selection for theme

--- home/home.js
+++ home/home.js
@@ -1,19 +1,26 @@
 import makeUser from '../home/make-user.js';
 import { saveUser } from '../common/utils.js';
 
+const userThemeForm = document.getElementById('theme-selection');
+
+userThemeForm.addEventListener('change', function(){
+    const userThemeSelection = userThemeForm.querySelector('input:checked').value;
+    document.getElementById(userThemeSelection).classList.remove('hidden');
+    const notUserThemeSelection = userThemeForm.querySelector('input:not(:checked)').value;
+    document.getElementById(notUserThemeSelection).classList.add('hidden');
+    localStorage.setItem('theme', userThemeSelection);
+});
+
 const userInfoForm = document.getElementById('user-info-form');
 userInfoForm.addEventListener('submit', processForm);
-
 function processForm(event) {
     event.preventDefault();
     const formData = new FormData(userInfoForm);
     const user = formData.get('name');
     const boardSize = formData.get('board-size');
     const difficulty = formData.get('difficulty');
-    const theme = formData.get('theme');
     localStorage.setItem('board-size', boardSize);
     localStorage.setItem('difficulty', difficulty);
-    localStorage.setItem('theme', theme);
     const userObject = makeUser(user);
     saveUser(userObject);
     window.location = './game/index.html';
