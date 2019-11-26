e516c9bc03e4697da60c95da1ce9c913ce53cc2b
okFox <smayfield@gmail.com>
2019-11-25 17:26:18 -0800

    added placeholder sounds

--- assets/sounds.js
+++ assets/sounds.js
@@ -1,8 +1,11 @@
-export const holdFlag = new Audio("bounce.mp3");
-export const placeFlag = new Audio("bounce.mp3");
-export const dropFlag = new Audio("bounce.mp3");
-export const tripMine = new Audio("bounce.mp3");
-export const recursion = new Audio("bounce.mp3");
-export const gameWin = new Audio("bounce.mp3");
-export const gameLoss = new Audio("bounce.mp3");
-export const clickAudio = new Audio("bounce.mp3");
+const theme = localStorage.getItem('theme');
+
+export const holdFlag = new Audio(`../assets/${theme}/audio/holdflag.mp3`);
+export const placeFlag = new Audio(`../assets/${theme}/audio/placeflag.mp3`);
+export const dropFlag = new Audio(`../assets/${theme}/audio/dropflag.mp3`);
+export const tripMine = new Audio(`../assets/${theme}/audio/tripmine.mp3`);
+export const recursion = new Audio(`../assets/${theme}/audio/recursion.mp3`);
+export const gameWin = new Audio(`../assets/${theme}/audio/gamewin.mp3`);
+//export const gameLoss = new Audio(`../assets/${theme}/audio/gameloss.mp3`);
+export const clickAudio = new Audio(`../assets/${theme}/audio/clickaudio.mp3`);
+
