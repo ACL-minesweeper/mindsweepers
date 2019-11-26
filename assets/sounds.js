a572ea5c9851be7fa00e88e2bfc7c0ae1b76d36a
okFox <smayfield@gmail.com>
2019-11-25 16:29:32 -0800

    sounds.js

---
+++ assets/sounds.js
@@ -0,0 +1,8 @@
+export const holdFlag = new Audio("bounce.mp3");
+export const placeFlag = new Audio("bounce.mp3");
+export const dropFlag = new Audio("bounce.mp3");
+export const tripMine = new Audio("bounce.mp3");
+export const recursion = new Audio("bounce.mp3");
+export const gameWin = new Audio("bounce.mp3");
+export const gameLoss = new Audio("bounce.mp3");
+export const clickAudio = new Audio("bounce.mp3");
