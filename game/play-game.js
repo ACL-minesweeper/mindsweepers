7cee258e7bd8d30976b79e60e99d22adbe759a80
drmartell <drmartell@gmail.com>
2019-11-28 10:57:13 -0800

    merged play game and game

--- game/play-game.js
+++ game/play-game.js
@@ -1,11 +1,9 @@
 import state from './state.js';
-import { cellClick } from './game.js';
-import { isWin, getUser, saveUser } from '../common/utils.js';
+// import './board-specs.js';
+import { boardSpecs } from './board-specs.js';
+import { isWin, getUser, saveUser, returnHomeIfNoUser } from '../common/utils.js';
 import { clearAdjCells } from './clear-adj-cells.js';
 import { holdFlag, placeFlag, dropFlag, tripMine, recursion, gameWin, clickAudio } from '../assets/sounds.js';   
-
-import { timerInterval } from './game.js';
-
 
 // populate flag info header
 const flagDiv = document.getElementById('flag-info');
@@ -36,6 +34,7 @@ export const playGame = () => {
     const objectRow = state.clickedCellArray[0];
     const objectColumn = state.clickedCellArray[1];
     const cellObject = state.boardArray[objectRow][objectColumn];
+    console.log('isHidden', cellObject.isHidden);
     const clickedCellIdString = state.clickedCellArray[0] + ',' + state.clickedCellArray[1];
     const domCell = document.getElementById(clickedCellIdString);
     // remove a flag from a flagged cell
@@ -156,3 +155,88 @@ function userWon(userWonBoolean) {
         userProfile.textContent = currentUser.user + ' you lost!';
     }
 }
+
+// get DOM elements
+const mainContainer = document.getElementById('main-container');
+const userProfile = document.getElementById('profile-user-name');
+const timerDiv = document.getElementById('timer');
+const playAgainButton = document.getElementById('play-again-button');
+
+//updating DOM with user profile (in this case, just the user name)
+const currentUser = getUser();
+returnHomeIfNoUser(currentUser);
+// prevent console errors
+if (currentUser) userProfile.textContent = currentUser.user;
+
+export let timerInterval;
+
+const boardDimension = boardSpecs.boardDimension[localStorage.getItem('board-size')];
+mainContainer.style.setProperty('--numRows', boardDimension);
+mainContainer.style.setProperty('--numColumns', boardDimension);
+
+const setBlankBoard = () => {
+    state.boardArray.forEach(row =>
+        row.forEach(cell =>
+            createCell(cell.id)));
+};
+
+export const incrementTimeDiv = timerInterval => {
+    const currentTime = +timerDiv.textContent;
+    timerDiv.textContent = (currentTime + 1).toString().padStart(3, '0');
+    if (currentTime >= 998) clearInterval(timerInterval.id);
+};
+
+// handles user click if firstClick and otherwise
+export const cellClick = event => {
+    state.updateClickedCellArray(event.target.id);
+    if (state.firstClick) {
+        // after the first click, board objects are updated with mines and numAdjines   
+        state.initializeDreamBoardArray();
+        state.firstClick = false;
+        event.target.classList.remove('opacity');
+        state.boardArray[state.clickedCellArray[0]][state.clickedCellArray[1]].isHidden = false;
+        //state.updateClickedCellArray(firstCell.id);
+        clearAdjCells(state.clickedCellArray);
+        timerInterval = setInterval(() => incrementTimeDiv(timerInterval.id), 1000);
+    } else {
+        playGame();
+    }
+};
+
+// create DOM cell div elements
+const createCell = id => {
+    const newDiv = document.createElement('div');
+    newDiv.id = id;
+    // to remove all of the classes every time the board is set 
+    newDiv.className = '';
+    newDiv.classList.add('opacity');
+    mainContainer.appendChild(newDiv);
+    newDiv.addEventListener('click', cellClick);
+};
+// initialize blank conceptual board, initialize flagsRemaining, and setup board for first click
+state.initializeBlankBoardArray();
+//state.initializeFlagsRemaining();
+setBlankBoard();
+
+const playAgain = mainContainerParam => {
+    // clear the board container so we can place a brand new board, strips 
+    mainContainerParam.innerHTML = '';
+    // reinitialize firstClick for user
+    state.firstClick = true;
+    // reset timerDiv text
+    clearInterval(timerInterval);
+    timerDiv.textContent = '000';
+    // reset flags to full count which matches the number of mines
+    state.initializeFlagsRemaining();
+    state.userHasFlag = false; 
+    const flagDiv = document.getElementById('flag-info');
+    flagDiv.textContent = state.flagsRemaining;
+    flagDiv.className = 'flag-pre-click';
+    userProfile.textContent = currentUser.user;
+    // create a brand new conceptual board
+    state.initializeBlankBoardArray();
+    // create a brand new DOM board
+    setBlankBoard(state.boardArray);
+};
+
+playAgainButton.addEventListener('click', () => playAgain(mainContainer));
